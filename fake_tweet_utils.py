ead82c6985f2745070944fceb050d33031165b62
Alex Rodriguez <126417914+al3xrods@users.noreply.github.com>
2023-03-21 22:53:00 +0200

    Add files via upload

---
+++ fake_tweet_utils.py
@@ -0,0 +1,32 @@
+
+# CHECKING LANGUAGUE OF TEXT.
+from langdetect import detect
+
+english = []
+with open("a.txt", 'w', encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        try:
+            if detect(line) =='en':
+                f.write(line)
+            else:
+                continue
+        except:
+            continue
+
+# =============================================================================================================================================
+# FINDING NOT WANTED WORDS => REMOVING LINE.
+bad_words = ['bad', 'naughty']
+with open('b.txt', 'r', encoding="utf-8") as oldfile, open('a.txt', 'a', encoding="utf-8") as newfile:
+    for line in oldfile:
+        if not any('Holiday' in line for bad_word in bad_words):
+            newfile.write(line)
+
+#=============================================================================================================================================
+
+#REMOVING LINES LESS THAT 180 CHARACHTERS.
+with open('a.txt', 'r', encoding="utf-8") as f:
+    for line in oldfile.readlines():
+        text = line.split('―')[0]
+        if len(text) < 180:
+            f.write(line)
---
+++ get_quotes.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+
+quotes = []
+s = HTMLSession()
+LASTPAGE = 88
+for i in range(1, LASTPAGE):
+    URL = f'https://www.goodreads.com/author/quotes/17212.Marcus_Aurelius?page={i}'
+    r = s.get(URL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    data = soup.find_all('div', class_='quoteText')
+    with open('marcus.txt', 'w', encoding="utf-8") as f:
+        for line in data:
+            quote = line.text
+            f.write(quote)
+
