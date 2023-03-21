33fc9ff8fd9e372c386edf54018d354504f424f6
Alex Rodriguez <126417914+al3xrods@users.noreply.github.com>
2023-03-21 22:54:48 +0200

    Update and rename get_quotes.py to scrape_quotes.py

--- scrape_quotes.py
+++ scrape_quotes.py
@@ -3,13 +3,13 @@ from requests_html import HTMLSession
 
 quotes = []
 s = HTMLSession()
-LASTPAGE = 88
+LASTPAGE = 88 #Last Page on Website
 for i in range(1, LASTPAGE):
     URL = f'https://www.goodreads.com/author/quotes/17212.Marcus_Aurelius?page={i}'
     r = s.get(URL)
     soup = BeautifulSoup(r.text, 'html.parser')
     data = soup.find_all('div', class_='quoteText')
-    with open('marcus.txt', 'w', encoding="utf-8") as f:
+    with open('scraped.txt', 'w', encoding="utf-8") as f:
         for line in data:
             quote = line.text
             f.write(quote)
